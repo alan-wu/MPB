2fcc303fbb3cb9687736557374c11870c893ffe6
alan-wu <alan.wu@auckland.ac.nz>
2018-03-01 10:45:34 +1300

    Update the ZincJS library.

---
+++ js/zinc/glyph.js
@@ -0,0 +1,78 @@
+/**
+ * Zinc representation of glyph graphic, it contains the colours, 
+ * geometry and transformation of the glyph.
+ * 
+ * @param {THREE.Geometry} geometry - Geometry of the glyph.
+ * @param {THREE.material} materialIn - Material of the glyph.
+ * @param {Number} idIn - Ud of the glyph.
+ * 
+ * @class
+ * @author Alan Wu
+ * @return {Zinc.Glyph}
+ */
+Zinc.Glyph = function(geometry, materialIn, idIn)  {
+	var material = materialIn.clone();
+	material.vertexColors = THREE.FaceColors;
+	var mesh = new THREE.Mesh( geometry, material );
+	
+	this.id = idIn;
+	var _this = this;
+	
+	/**
+	 * Get the mesh of this glyph.
+	 * @return {THREE.Mesh}
+	 */
+	this.getMesh = function () {
+		return mesh;
+	}
+	
+	/**
+	 * Get the bounding box of this glyph.
+	 * @return {THREE.Box3}
+	 */
+	this.getBoundingBox = function() {
+		if (mesh)
+			return new THREE.Box3().setFromObject(mesh);
+		return undefined;
+	}
+	
+	/**
+	 * Set the Colour of this glyph.
+	 * @param {THREE.Color} colorIn - Colour to be set of this mesh.
+	 */
+	this.setColor = function (colorIn) {
+		mesh.material.color = colorIn;
+		mesh.geometry.colorsNeedUpdate = true;
+	}
+	
+	/**
+	 * Set the transformation of this glyph.
+	 * @param {Array} position - Three components vectors containing position of the
+	 * transformation.
+	 * @param {Array} axis1 - Three components vectors containing axis1 rotation of the
+	 * transformation.
+	 * @param {Array} axis2 - Three components vectors containing axis2 rotation of the
+	 * transformation.
+	 * @param {Array} position - Three components vectors containing axis3 rotation of the
+	 * transformation.
+	 */
+	this.setTransformation = function(position, axis1, axis2, axis3) {
+		mesh.matrix.elements[0] = axis1[0];
+		mesh.matrix.elements[1] = axis1[1];
+		mesh.matrix.elements[2] = axis1[2];
+		mesh.matrix.elements[3] = 0.0;
+		mesh.matrix.elements[4] = axis2[0];
+		mesh.matrix.elements[5] = axis2[1];
+		mesh.matrix.elements[6] = axis2[2];
+		mesh.matrix.elements[7] = 0.0;
+		mesh.matrix.elements[8] = axis3[0];
+		mesh.matrix.elements[9] = axis3[1];
+		mesh.matrix.elements[10] = axis3[2];
+		mesh.matrix.elements[11] = 0.0;
+		mesh.matrix.elements[12] = position[0];
+		mesh.matrix.elements[13] = position[1];
+		mesh.matrix.elements[14] = position[2];
+		mesh.matrix.elements[15] = 1.0;
+		mesh.matrixAutoUpdate = false;
+	}	
+}
---
+++ js/zinc/zinc.js
@@ -0,0 +1,11 @@
+/**
+ * Provides a global namespace for the Zinc javascript library and some default parameters for it.
+ * 
+ * @namespace
+ * @author Alan Wu
+ */
+var Zinc = { REVISION: '27' };
+
+
+Zinc.defaultMaterialColor = 0x7F1F1A;
+Zinc.defaultOpacity = 1.0;
